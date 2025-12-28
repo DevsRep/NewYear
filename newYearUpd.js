53cbef45a6f73030b9192583f45b2f9078398326
Devesh Keshavan S <thedevrepository@gmail.com>
2025-12-28 23:26:53 +0530

    Update countdown for New Year 2026 and improve timer
    Updated all references from 2025 to 2026 in index.html and newYearUpd.js. Adjusted the countdown end date to December 31, 2025. Enhanced the countdown display to show days remaining when applicable. Reduced the timer font size for better responsiveness. Added a function to extract a 'name' parameter from the URL.

--- newYearUpd.js
+++ newYearUpd.js
@@ -5,6 +5,18 @@ const secHand = document.querySelector(".sec")
 const timerto2025 = document.querySelector(".timer-o-cont")
 const mainNY = document.querySelector(".newYear")
 
+
+function getNameFromUrl(){
+    const queryString = window.location.search
+    const urlParam = new URLSearchParams(queryString)
+    if(urlParam.get("name")){
+        console.log(urlParam.get("name"))
+    }
+}
+
+getNameFromUrl()
+
+
 function clockTick(){
 
     let date = new Date()
@@ -25,7 +37,7 @@ clockTick()
 
 timeele=document.querySelector(".time");
 dateele=document.querySelector(".date");
-let endDate = new Date('December 31, 2024 24:00:00');
+let endDate = new Date('December 31, 2025 24:00:00');
 let endTime = endDate.getTime();
 
 
@@ -34,9 +46,11 @@ function countdown() {
     let todayDate = new Date();
     let todayTime = todayDate.getTime();
     let remainingTime = endTime - todayTime;
+    // console.log(remainingTime)
     let oneMin = 60 * 1000;
     let oneHr = 60 * oneMin;
     let oneDay = 24 * oneHr;
+    let oneWeek = oneDay * 7;
     if (endTime <= todayTime) {
         clearInterval(i);
         document.querySelector(".time").innerHTML = '<h1>Countdown Has Expired</h1>';
@@ -44,11 +58,18 @@ function countdown() {
         mainNY.style.display = "grid"
         // aftermid.style.display="flex";
     } else {
+        let daysLeft = Math.floor(remainingTime / oneDay);
         let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
         let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
         let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
-        timeele.textContent=("0" + hrsLeft).slice(-2) + " : " +("0"+minsLeft).slice(-2) + " : " + ("0"+secsLeft).slice(-2);
+        if(daysLeft>0){
+            timeele.textContent=("0" + daysLeft).slice(-2) + " : " + ("0" + hrsLeft).slice(-2) + " : " +("0"+minsLeft).slice(-2) + " : " + ("0"+secsLeft).slice(-2);
+        }else{
+            timeele.textContent=("0" + hrsLeft).slice(-2) + " : " +("0"+minsLeft).slice(-2) + " : " + ("0"+secsLeft).slice(-2);
+
+        }
     }
+
 }
 let i = setInterval(countdown, 1000);
 countdown();
